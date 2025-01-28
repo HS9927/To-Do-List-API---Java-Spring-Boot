6fe8c440a5994178aeb382926e70db522b7ab0c8
HS9927 <hengsok2202@gmail.com>
2025-01-28 19:01:30 +0700

    frist commit

---
+++ src/main/java/com/project/To_Do_List_API___Java_Spring_Boot/ToDoListApiJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.project.To_Do_List_API___Java_Spring_Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListApiJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListApiJavaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/To_Do_List_API___Java_Spring_Boot/ToDoListApiJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.To_Do_List_API___Java_Spring_Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApiJavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
